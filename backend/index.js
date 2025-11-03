e83322970e66a1986b82e63d36c773e19887b261
Santiago9111 <santiago.medinar@davinci.edu.ar>
2025-11-03 13:01:43 -0300

    aplicaciones_hibiridas_medina_morrone_tp2

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
 import humanRoutes from "./routes/HumanRoutes.js";
@@ -12,6 +13,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use( cors());
 
 app.get("/", (req, res) => {
   res.send(`
